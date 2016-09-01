63eaa9d17d577db10b3b6ce021ba106e581ff954
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-01 11:12:23 -0500

    Add project files.

---
+++ TestCrud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCrud.Startup))]
+namespace TestCrud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
