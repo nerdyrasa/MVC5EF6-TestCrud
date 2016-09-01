c673e204e5234757fb7be0d4c97ba20bef8e8d77
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-01 11:26:37 -0500

    Add Thing model.

---
+++ TestCrud/Models/Thing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestCrud.Models
+{
+    public class Thing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
